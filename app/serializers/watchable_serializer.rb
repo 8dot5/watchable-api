3f2e0fc9c2ff2513b41f498898f42c13d7dfb16b
Chun Yi <8.5by11@gmail.com>
2022-04-08 21:45:27 -0400

    Added updated_at

--- app/serializers/watchable_serializer.rb
+++ app/serializers/watchable_serializer.rb
@@ -1,5 +1,5 @@
 class WatchableSerializer < ActiveModel::Serializer
-  attributes :id, :title, :rating, :summary, :poster_url, :trailer_url, :favorite, :category_id, :user_id
+  attributes :id, :title, :rating, :summary, :poster_url, :trailer_url, :favorite, :category_id, :user_id, :updated_at
 
   #:service_id,
   belongs_to :category
