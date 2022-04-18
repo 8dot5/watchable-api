52bfc4694a13a2142306228f5b1defe7aba44ad2
Chun Yi <8.5by11@gmail.com>
2022-04-18 11:52:39 -0400

    Modified application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,42 @@
 class ApplicationController < ActionController::API
-    include ActionController::Cookies
+  before_action :set_csrf_cookie #
+  include ActionController::RequestForgeryProtection #
+  include ActionController::Cookies
 
-    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
-    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  protect_from_forgery with: :exception
 
-    # test route:  http://localhost:3000/hello
-    def hello_world
-      session[:count] = (session[:count] || 0) + 1
-      render json: { count: session[:count] }
-    end
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-    # TODO refactor this later
-    # def current_user
-    #   User.find_by(id: session[:user_id])
-    # end
+  # test route:  http://localhost:3000/hello
+  def hello_world
+    session[:count] = (session[:count] || 0) + 1
+    render json: { count: session[:count] }
+  end
 
-    private
-    def record_not_found_response
-      render json: { errors: "Not found" }, status: :not_found
-    end
+  def cookie
+    "ok"
+  end
 
-    def unprocessable_entity_response(response)
-      render json: { errors: response.record.errors.full_messages }, status: :unprocessable_entity
-    end
+  # TODO refactor this later
+  # def current_user
+  #   User.find_by(id: session[:user_id])
+  # end
+
+  private
+  def record_not_found_response
+    render json: { errors: "Not found" }, status: :not_found
+  end
+
+  def unprocessable_entity_response(response)
+    render json: { errors: response.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def set_csrf_cookie
+    cookies["CSRF-TOKEN"] = {
+         value: form_authenticity_token,
+         domain: :all
+     }
+ end
 
 end
